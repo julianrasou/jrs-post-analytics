885cd93e025ba0176461df83b0931ee3e36f9f98
julianrasou <julianrasou03@gmail.com>
2025-05-07 20:14:20 +0200

    Added base structure for mail class file

--- class-jrs-post-analytics.php
+++ class-jrs-post-analytics.php
@@ -9,7 +9,60 @@
 
 /**
  * Contains the plugin's functionality
+ *
+ * @since             1.0.0
  */
 class Jrs_Post_Analytics {
 
+	/**
+	 * Summary of __construct
+	 * initializes the settings menu and the filter to add statistics to posts
+	 */
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'add_tools_page' ) );
+		add_action( 'admin_init', array( $this, 'add_settings' ) );
+		add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
+		add_filter( 'the_content', array( $this, 'if_settings_enabled' ) );
+	}
+
+	/**
+	 * Adds a submenu page to the Tools main menu.
+	 */
+	private function add_tools_page() {
+		add_management_page(
+			__( 'Post Analytics Settings', 'jrs-post-analytics' ),
+			__( 'Post Analytics', 'jrs-post-analytics' ),
+			'manage_options',
+			'post-analytics-settings-page',
+			array( $this, 'build_settings_page_html' ),
+			null
+		);
+	}
+
+	/**
+	 * Builds the content of the settings page.
+	 */
+	private function build_settings_page_html() {
+	}
+
+	/**
+	 * Adds the settings fields to the settigs page.
+	 */
+	private function add_settings() {
+	}
+
+	/**
+	 * Load plugin text domain.
+	 */
+	private function load_textdomain() {
+	}
+
+	/**
+	 * Checks if the settings options are enabled, if they are:
+	 * Adds post analytics at the preffered location.
+	 *
+	 * @param string $content Content of the current post.
+	 */
+	private function if_settings_enabled( $content ) {
+	}
 }
